8a05f7f2f1967a78d0e93083c8581b06222c87d5
fukamachi <e.arrows@gmail.com>
2010-07-06 17:23:01 +0900

    fixed `def-find' couldn't generate the where form when given nil for the value

--- core.lisp
+++ core.lisp
@@ -7,19 +7,20 @@
                        (loop for x in args
                              collect (list (concat-symbol-pkg :KEYWORD x) x))))))
 
-(defmacro build-where (&rest args)
-  `(clsql:sql-operation
-    'and
-    ,@(loop for (k v) in args
+(defun build-where (&rest args)
+  (apply #'clsql:sql-operation 'and
+         (loop for (k v) in args
             unless (null v)
-            collect `(clsql:sql-operation '=
-                                          (clsql:sql-expression :attribute ,k) ,v))))
+            collect (clsql:sql-operation
+                     '=
+                     (clsql:sql-expression :attribute
+                                           (concat-symbol-pkg :KEYWORD k)) v))))
 
 (defmacro def-find (table args)
   `(defun ,(concat-symbol 'find- table) (&key ,@args)
      (clsql:select ,@(mapcar #'(lambda (a) `(quote ,a)) args)
                    :from (clsql:sql-expression :table ',table)
-                   :where (build-where ,@(loop for x in args collect (list (concat-symbol-pkg :KEYWORD x) x))))))
+                   :where (build-where ,@(mapcar #'(lambda (a) `(list ',a ,a)) args)))))
 
 (def-create user (name pass))
 (def-create task (body))
